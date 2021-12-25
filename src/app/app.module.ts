27ccf2289bcd282e3c69660d29d8eda9ccadc543
Charles.Ek <macbook@MacBook.local>
2021-12-25 05:08:41 +0100

    :sparkles:user-avat-componet neumorphism style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
 import { AvatarComponent } from './avatar/avatar.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenuIconComponent } from './navbar/menu-icon/menu-icon.component';
 import { MainComponent } from './main/main.component';
-import { MediaQueryService } from './services/media-query/media-query.service';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -30,8 +32,25 @@ import { MediaQueryService } from './services/media-query/media-query.service';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    HttpClientModule
   ],
-  providers: [MediaQueryService],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  path = './assets/icons';
+
+  constructor(private matIconRegistery : MatIconRegistry, private sanitizer : DomSanitizer ){
+
+    this.matIconRegistery
+    .addSvgIcon('equalizer', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/equalizer.svg`) )
+    .addSvgIcon('favorites', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/favorites.svg`) )
+    .addSvgIcon('home', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/home.svg`) )
+    .addSvgIcon('log_out', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/log_out.svg`) )
+    .addSvgIcon('mic', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/mic.svg`) )
+    .addSvgIcon('playlist', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/playlist.svg`) )
+    .addSvgIcon('settings', this.sanitizer.bypassSecurityTrustResourceUrl(`${this.path}/settings.svg`) )
+  }
+ 
+}
--- src/app/sidebar/sidebar-item/sidebar-item.component.ts
+++ src/app/sidebar/sidebar-item/sidebar-item.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SidebarItemComponent implements OnInit {
   @Input() label : string ;
   @Input() icon :string;
-  
+
   constructor() { 
     this.icon = ''
     this.label = ''
